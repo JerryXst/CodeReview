41e42cecf4eb1415c0a730691b920d9e2499a5c8
Jerry Xst <jerry_xst@live.cn>
2016-02-22 19:29:27 +0800

    change Window layout

--- CodeReview/SettingWindowController.m
+++ CodeReview/SettingWindowController.m
@@ -18,6 +18,7 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    [self updateView];
 }
 
 - (void)updateView
